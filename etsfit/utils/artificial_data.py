f34fc2fb9346676970261328450e27109bec5ee5
lcgordon <contactlindseycg@gmail.com>
2023-01-28 11:44:36 -0600

    updates to injection noise

--- etsfit/utils/artificial_data.py
+++ etsfit/utils/artificial_data.py
@@ -13,6 +13,7 @@ Upated: 1/25/23 LG
 import numpy as np
 import matplotlib.pyplot as plt
 from etsfit import etsMAIN
+import etsfit.utils.utilities as ut
 from pylab import rcParams
 rcParams['figure.figsize'] = 8,3
 
@@ -25,68 +26,65 @@ class artificial_lc(object):
         self.save_dir = save_dir
         
         
-    def gen_fakes(self, n):
+    def gen_fakes(self, n, tessreducefile, TNSFile):
         """ 
         
         Note to self: for paper purposes, expand out the limits on the 
         params to go past the priors
+        n = # to make
+        tessreducefile = Ia whose noise model ur gonna generate
+        TNSFile = u know
         
         """
         print("making parameter vectors")
-        def func1(x, t0, t1, A1, A2, beta1, beta2):
-            return A1 *(x-t0)**beta1
-        def func2(x, t0, t1, A1, A2, beta1, beta2):
-            return A1 * (x-t0)**beta1 + A2 * (x-t1)**beta2
         
         self.n = n
-        #t0, t1, A0, A1, beta0, beta1, B
-        self.params_true = np.zeros((self.n, 7))
-        self.l = 1_000
-        self.x = np.linspace(0, 28, self.l)
-        self.flux_fake = np.ones((self.n, (self.l-100)))
-        self.error_fake = np.ones((self.n, (self.l-100)))
+        #t0 A beta B
+        self.params_true = np.zeros((self.n, 4)) #7
+        # make the noise model
+        self.__tess_noise(tessreducefile, TNSFile)
         
-        self.params_true[:,0] = np.random.uniform(1, 28, self.n) #t0
-        self.params_true[:,2] = np.random.uniform(0.01, 5, self.n) #A1
-        
-        self.params_true[:,4] = np.random.uniform(0.5, 3, self.n) #beta1
-        
-        self.params_true[:,6] = np.random.uniform(-30, 30, self.n) #B
-        
-        #remove the middles like it's an orbit gap
+        # x axis with a fake orbit gap
+        self.x = np.linspace(0, 28, self.l+100)
         #indexes 475 (13.31) to 575 (16.11)
         mask = np.ones(self.l)
         mask[475:575] = 0
         mask = np.nonzero(mask) # which ones you are keeping
-        #mask x as well
         self.x = self.x[mask]
         
-        for i in range(self.n):
-            if not i%4: #do like a quarter of them
-                self.params_true[i][1] = np.random.uniform(self.params_true[i][0], 28, 1)
-                self.params_true[i][3] = np.random.uniform(0.01, 5, 1) #A2
-                self.params_true[i][5] = np.random.uniform(0.5, 3, 1) #beta2
-                t0, t1, A1, A2, beta1, beta2, B = self.params_true[i]
-                self.flux_fake[i] = np.piecewise(self.x, [(t0 <= self.x)*(self.x < t1), t1 <= self.x], 
-                                     [func1, func2], t0, t1, A1, A2, beta1, beta2) + 1 + B
-                
-            else:
-                t0, t1, A1, A2, beta1, beta2, B = self.params_true[i]
-                t_ = self.x - t0
-                self.flux_fake[i] = (np.heaviside((t_), 1) * A1 *np.nan_to_num((t_**beta1))) + 1 + B
+        self.flux_fake = np.ones((self.n, (self.l))) # n l-length arrays
+        self.error_fake = np.ones((self.n, (self.l)))
         
-            #put some noise on those guys:
-            #gaussian white noise + uniform white noise
-            #uniform noise: 5% max
-            unif = np.random.uniform(0,0.05*max(self.flux_fake[i]), self.l-100)
-            # gaussian noise: gaussian * 5% of max
-            gauss = np.random.normal(loc=0,
-                                     scale = 1, size=self.l-100) * 0.05*max(self.flux_fake[i])
-            #error? 
-            err = unif + gauss
-            expectation_val = np.mean(err)
-            self.flux_fake[i] = self.flux_fake[i] + err
-            self.error_fake[i] = expectation_val
+        self.params_true[:,0] = np.random.uniform(1, 28, self.n) #t0
+        self.params_true[:,1] = np.random.uniform(0.001, 5, self.n) #A1
+        self.params_true[:,3] = np.random.uniform(-20, 20, self.n) #B
+        # beta is pulled from a unif distro on the arctans
+        self.params_true[:,2] = np.tan(np.random.uniform(np.arctan(0.5), np.arctan(6), self.n))
+        #self.params_true[:,2] = np.random.uniform(0.5, 3, self.n) #beta1
+        
+        
+        
+        
+        for i in range(self.n):
+            t0, A, beta, B = self.params_true[i]
+            t_ = self.x - t0
+            self.flux_fake[i] = (np.heaviside((t_), 1) * A *np.nan_to_num((t_**beta))) + 1 + B
+            
+            #attach noise: 
+            self.flux_fake[i] = self.flux_fake[i] + self.noise_model
+            # set error = expectation value of noise model
+            self.error_fake[i] += np.mean(self.noise_model)
+        
+            # #put some noise on those guys:
+            # #gaussian white noise + uniform white noise
+            # #uniform noise: 5% max
+            # unif = np.random.uniform(0,0.05*max(self.flux_fake[i]), self.l-100)
+            # # gaussian noise: gaussian * 5% of max
+            # gauss = np.random.normal(loc=0,
+            #                          scale = 1, size=self.l-100) * 0.05*max(self.flux_fake[i])
+            # #error? 
+            # err = unif + gauss
+            # expectation_val = np.mean(err)
             
             #end for
 
@@ -101,6 +99,40 @@ class artificial_lc(object):
             self.disctimes[labels[i]] = dtimes[i]
         
         
+        return
+    
+    def __tess_noise(self, tessreducefile, TNSFile):
+        """ 
+        Generate a noise model to use for each target that you have
+        """
+        print("making noise model from data")
+        #load tess data
+        (time, flux, error, targetlabel, 
+             sector, camera, ccd) = ut.tr_load_lc(tessreducefile)
+        discoverytime = ut.get_disctime(TNSFile, targetlabel)
+        #run it once with a type 1
+        self.trlc = etsMAIN(self.save_dir, TNSFile)
+        
+        self.trlc.load_single_lc(time, flux, error, discoverytime, 
+                           targetlabel, sector, camera, ccd)
+
+        winfilter = self.trlc.window_rms_filt(plot=False)
+        self.trlc.pre_run_clean(1, flux_mask=winfilter)
+        #trlc.test_plot()
+        self.trlc.run_MCMC(5000, 25000, quiet=True)
+        #cut to just data prior to the t0 
+        print("t0 is: ", self.trlc.best_mcmc[0][0])
+        self.t_lim = np.nonzero(np.where(self.trlc.time <= self.trlc.best_mcmc[0][0], 1, 0))
+
+        self.cut_time = self.trlc.time[self.t_lim]
+        self.cut_flux = self.trlc.flux[self.t_lim]
+        #plt.scatter(self.cut_time, self.cut_flux)
+        #relocate to mean=0
+        m = np.mean(self.cut_flux)
+        self.cut_flux -= m
+        #draw samples from that to fill in a full fake light curve
+        self.l = len(time)
+        self.noise_model = np.random.choice(self.cut_flux, self.l)
         return
     
     
@@ -124,6 +156,8 @@ class artificial_lc(object):
         self.output_u_1 = np.zeros((self.n, 4)) #upper error
         self.output_l_1 = np.zeros((self.n, 4)) #lower error
         self.bic_1 = np.zeros((self.n, 1))
+        self.isright_1 = np.zeros((self.n, 4))
+        
         for i in range(start, self.n):
             dt = self.disctimes[i]
             trlc = etsMAIN(self.save_dir, 'nofile')
@@ -142,7 +176,15 @@ class artificial_lc(object):
             self.output_u_1[i] = trlc.upper_error[0]
             self.output_l_1[i] = trlc.lower_error[0]
             print(trlc.BIC)
-            self.bic_1[0] = trlc.BIC
+            self.bic_1[i] = trlc.BIC
+            
+            #comp with true
+            for j in range(4):
+                low = self.output_p_1[i][j]-self.output_l_1[i][j]
+                high = self.output_p_1[i][j]+self.output_u_1[i][j]
+                tru = self.params_true[i][j*2]
+                if (tru <= high) and (tru >= low) :
+                    self.isright_1[i][j] = 1
         return
     
     def fit_fakes_3(self, start=0, n1=500, n2=5000):  
@@ -151,6 +193,7 @@ class artificial_lc(object):
         self.output_u_3 = np.zeros((self.n, 7)) #upper error
         self.output_l_3 = np.zeros((self.n, 7)) #lower error
         self.bic_3 = np.zeros((self.n, 1))
+        self.isright_3 = np.zeros((self.n, 7))
         for i in range(start, self.n):
             dt = self.disctimes[i]
             trlc = etsMAIN(self.save_dir, 'nofile')
@@ -168,17 +211,31 @@ class artificial_lc(object):
             self.output_p_3[i] = trlc.best_mcmc[0]
             self.output_u_3[i] = trlc.upper_error[0]
             self.output_l_3[i] = trlc.lower_error[0]
-            self.bic_3[0] = trlc.BIC
+            self.bic_3[i] = trlc.BIC
+            
+            #comp with true
+            for j in range(4):
+                low = self.output_p_3[i][j]-self.output_l_3[i][j]
+                high = self.output_p_3[i][j]+self.output_u_3[i][j]
+                tru = self.params_true[i][j]
+                if (tru <= high) and (tru >= low) :
+                    self.isright_3[i][j] = 1
         return
+    
+    
+
+        
+        
+    
         
    
         
-            
-        
+TNSFile = "/Users/lindseygordon/research/urop/august2022crossmatch/tesscut-Ia18th.csv"           
+tessreducefile = "/Users/lindseygordon/research/urop/tessreduce_lc/2020tld2921/2020tld2921-tessreduce"        
  
 lc = artificial_lc("./research/urop/fake_data/")
-lc.gen_fakes(10)
+lc.gen_fakes(10, tessreducefile, TNSFile)
 # lc.plot_fake(1)
-lc.fit_fakes_1()
-lc.fit_fakes_3()
+#lc.fit_fakes_1()
+#lc.fit_fakes_3()
         
